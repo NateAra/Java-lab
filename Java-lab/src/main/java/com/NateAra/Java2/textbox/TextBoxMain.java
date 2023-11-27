d3a42996f0532856ae4c77488137f9ac102a2ea8
NateAra <nate.arafayne@appliedtechnology.se>
2023-11-27 13:19:33 +0100

    Added a main for textBox experimenting, for further separation between experiments

---
+++ Java-lab/src/main/java/com/NateAra/Java2/textbox/TextBoxMain.java
@@ -0,0 +1,12 @@
+package com.NateAra.Java2.textbox;
+
+public class TextBoxMain {
+
+    public static void main(String[] args) {
+
+            var textBox1 = new TextBox();
+            textBox1.setText("Hello World!");
+            System.out.println(textBox1.text);
+
+    }
+}
