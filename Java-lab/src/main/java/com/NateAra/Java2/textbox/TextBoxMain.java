6b570d23da9f61d4c453ab52bce7d42a4a4ed6db
NateAra <nate.arafayne@appliedtechnology.se>
2023-11-27 13:27:35 +0100

    Experimental

--- Java-lab/src/main/java/com/NateAra/Java2/textbox/TextBoxMain.java
+++ Java-lab/src/main/java/com/NateAra/Java2/textbox/TextBoxMain.java
@@ -4,9 +4,13 @@ public class TextBoxMain {
 
     public static void main(String[] args) {
 
-            var textBox1 = new TextBox();
-            textBox1.setText("Hello World!");
-            System.out.println(textBox1.text);
+        var textBox1 = new TextBox();
+        textBox1.setText("Hello World!");
+        System.out.println(textBox1.text.toUpperCase());
+
+        var textBox2 = new TextBox();
+        textBox2.setText("Hey Nate!");
+        System.out.println(textBox2.text.toUpperCase());
 
     }
 }
