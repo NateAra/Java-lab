4e88c9b9e3f15c6a68261e7d9136f512a84d89ce
NateAra <nate.arafayne@appliedtechnology.se>
2023-11-27 11:51:24 +0100

    Creating a basic Object

---
+++ Java-lab/src/main/java/com/NateAra/Java2/textbox/TextBox.java
@@ -0,0 +1,14 @@
+package com.NateAra.Java2.textbox;
+
+public class TextBox {
+
+    public String text;
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public void clear() {
+        text = "";
+    }
+}
