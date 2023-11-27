3f5ebe5e74195bcaddfda984c5cc40f0b96e7311
NateAra <nate.arafayne@appliedtechnology.se>
2023-11-27 13:13:37 +0100

    initialized text field

--- Java-lab/src/main/java/com/NateAra/Java2/textbox/TextBox.java
+++ Java-lab/src/main/java/com/NateAra/Java2/textbox/TextBox.java
@@ -2,7 +2,7 @@ package com.NateAra.Java2.textbox;
 
 public class TextBox {
 
-    public String text;
+    public String text = "";
 
     public void setText(String text) {
         this.text = text;
