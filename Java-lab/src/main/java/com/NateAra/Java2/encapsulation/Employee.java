0e134c088a5ee06538180b31ebee98f6b3fd8ded
NateAra <nate.arafayne@appliedtechnology.se>
2023-11-29 11:42:42 +0100

    Added getters and setters

--- Java-lab/src/main/java/com/NateAra/Java2/encapsulation/Employee.java
+++ Java-lab/src/main/java/com/NateAra/Java2/encapsulation/Employee.java
@@ -2,10 +2,33 @@ package com.NateAra.Java2.encapsulation;
 
 public class Employee {
 
-    public double baseSalary;
-    public double hourlyRate;
+    private double baseSalary;
+    private double hourlyRate;
 
     public double calculateWage(double extraHours ) {
         return baseSalary + (hourlyRate * extraHours);
+    }
+
+    public double setBaseSalary(double baseSalary) {
+        if (baseSalary <= 0) {
+            throw new IllegalArgumentException("Base Salary has to be more than 0");
+        }
+        return this.baseSalary = baseSalary;
+    }
+
+
+    public double getBaseSalary() {
+        return baseSalary;
+    }
+
+    public void setHourlyRate(double hourlyRate) {
+        if (hourlyRate <= 0) {
+            throw new IllegalArgumentException("Hourly Rate has to be more than 0");
+        }
+        this.hourlyRate = hourlyRate;
+    }
+
+    public double getHourlyRate() {
+        return hourlyRate;
     }
 }
