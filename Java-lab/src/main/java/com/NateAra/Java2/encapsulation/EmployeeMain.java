688a27d2e035a5b2d44bb7b360baa19895cb5555
NateAra <nate.arafayne@appliedtechnology.se>
2023-11-29 11:43:03 +0100

    Implemented getters and setters

--- Java-lab/src/main/java/com/NateAra/Java2/encapsulation/EmployeeMain.java
+++ Java-lab/src/main/java/com/NateAra/Java2/encapsulation/EmployeeMain.java
@@ -7,8 +7,9 @@ public class EmployeeMain {
     public static void main(String[] args) {
 
         var employee = new Employee();
-        employee.baseSalary = 50_000;
-        employee.hourlyRate = 20;
+        employee.setBaseSalary(50_000);
+        employee.setHourlyRate(20);
+        employee.getBaseSalary();
         double wage = employee.calculateWage(10);
 
         System.out.println(NumberFormat.getCurrencyInstance().format(wage));
