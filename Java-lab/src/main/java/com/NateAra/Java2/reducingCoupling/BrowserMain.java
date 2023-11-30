9fe8b66ae27fb27e546c46fad0e3f3d3592be8f5
NateAra <nate.arafayne@appliedtechnology.se>
2023-11-30 11:57:45 +0100

    Deleted Browser classes

--- Java-lab/src/main/java/com/NateAra/Java2/reducingCoupling/BrowserMain.java
+++
@@ -1,9 +0,0 @@
-package com.NateAra.Java2.reducingCoupling;
-
-public class BrowserMain {
-
-    public static void main(String[] args) {
-        var browser = new Browser();
-        browser.navigate("Reducing Coupling");
-    }
-}
