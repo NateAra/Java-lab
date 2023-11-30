24c71ef1ecc1a56a438eb75410688ff09f3ca49a
NateAra <nate.arafayne@appliedtechnology.se>
2023-11-30 10:49:36 +0100

    calling the browser with only the necessary methods or available methods

---
+++ Java-lab/src/main/java/com/NateAra/Java2/reducingCoupling/BrowserMain.java
@@ -0,0 +1,9 @@
+package com.NateAra.Java2.reducingCoupling;
+
+public class BrowserMain {
+
+    public static void main(String[] args) {
+        var browser = new Browser();
+        browser.navigate("Reducing Coupling");
+    }
+}
