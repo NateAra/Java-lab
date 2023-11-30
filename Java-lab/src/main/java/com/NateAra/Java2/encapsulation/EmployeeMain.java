3aca073efacac13edc74893972cd27a3d68a0644
NateAra <nate.arafayne@appliedtechnology.se>
2023-11-30 12:00:09 +0100

    Testing the employee constructor

--- Java-lab/src/main/java/com/NateAra/Java2/encapsulation/EmployeeMain.java
+++ Java-lab/src/main/java/com/NateAra/Java2/encapsulation/EmployeeMain.java
@@ -10,7 +10,11 @@ public class EmployeeMain {
         employee.setBaseSalary(50_000);
         employee.setHourlyRate(20);
         double wage = employee.calculateWage(10);
-
         System.out.println(NumberFormat.getCurrencyInstance().format(wage));
+
+        //Employee with a constructor
+        var employeeCon = new EmployeeCon(50_000, 20);
+        double wage2 = employeeCon.calculateWage(50);
+        System.out.println(NumberFormat.getCurrencyInstance().format(wage2));
     }
 }
