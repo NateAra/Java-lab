5c72d1213f0273fb3bd56db13c55cd020cc2a795
NateAra <nate.arafayne@appliedtechnology.se>
2023-11-30 11:59:38 +0100

    Added an employee class with a constructor

---
+++ Java-lab/src/main/java/com/NateAra/Java2/encapsulation/EmployeeCon.java
@@ -0,0 +1,39 @@
+package com.NateAra.Java2.encapsulation;
+
+public class EmployeeCon {
+
+    private double baseSalary;
+    private double hourlyRate;
+
+    public EmployeeCon(double baseSalary, double hourlyRate) {
+        setBaseSalary(baseSalary);
+        setHourlyRate(hourlyRate);
+    }
+
+    public double calculateWage(double extraHours) {
+        return baseSalary + (hourlyRate * extraHours);
+    }
+
+    public void setBaseSalary(double baseSalary) {
+        if (baseSalary <= 0) {
+            throw new IllegalArgumentException("Base Salary has to be more than 0");
+        }
+        this.baseSalary = baseSalary;
+    }
+
+
+    private double getBaseSalary() {
+        return baseSalary;
+    }
+
+    private double getHourlyRate() {
+        return hourlyRate;
+    }
+
+    public void setHourlyRate(double hourlyRate) {
+        if (hourlyRate <= 0) {
+            throw new IllegalArgumentException("Hourly Rate has to be more than 0");
+        }
+        this.hourlyRate = hourlyRate;
+    }
+}
