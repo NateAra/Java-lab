f4c9a32fd07b56b0f4472c42db3db40f1f2372cf
NateAra <nate.arafayne@appliedtechnology.se>
2023-12-03 15:48:40 +0100

    Basics of inheritance

--- Java-lab/src/main/java/com/NateAra/Java2/Inheritance/UIControl.java
+++ Java-lab/src/main/java/com/NateAra/Java2/Inheritance/UIControl.java
@@ -1,4 +1,18 @@
 package com.NateAra.Java2.Inheritance;
 
 public class UIControl {
+
+    private boolean isEnabled;
+
+    public boolean isEnabled() {
+        return isEnabled;
+    }
+
+    public void enable() {
+        isEnabled = true;
+    }
+
+    public void disable() {
+        isEnabled = false;
+    }
 }
