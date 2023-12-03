5764984d3ec287e4ee755bd8a4f1c070521aed6a
NateAra <nate.arafayne@appliedtechnology.se>
2023-12-03 13:03:42 +0100

    Refactored

--- Java-lab/src/main/java/com/NateAra/Java2/MortgageEx/MortgageCalculator.java
+++ Java-lab/src/main/java/com/NateAra/Java2/MortgageEx/MortgageCalculator.java
@@ -2,8 +2,8 @@ package com.NateAra.Java2.MortgageEx;
 
 public class MortgageCalculator {
 
-    public static final int MONTHS_IN_YEAR = 12;
-    public static final int PERCENT = 100;
+    private static final int MONTHS_IN_YEAR = 12;
+    private static final int PERCENT = 100;
     private final double principal;
     private final double annualInterestRate;
     private final double years;
@@ -20,8 +20,6 @@ public class MortgageCalculator {
 
         return principal * (monthlyInterestRate * Math.pow(1 + monthlyInterestRate, numberOfPayments)) / (Math.pow(1 + monthlyInterestRate, numberOfPayments) - 1);
     }
-
-
 
     public double calculateBalance(double numberOfPaymentsMade) {
         double monthlyInterestRate = getMonthlyInterestRate();
