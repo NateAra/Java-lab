f07c318941c41d46d52f68674c5b424f8a835324
NateAra <nate.arafayne@appliedtechnology.se>
2023-12-04 16:39:00 +0100

    Constructor and Inheritance

--- Java-lab/src/main/java/com/NateAra/Java2/Inheritance/UITextBox.java
+++ Java-lab/src/main/java/com/NateAra/Java2/Inheritance/UITextBox.java
@@ -4,6 +4,11 @@ public class UITextBox extends UIControl {
 
     private String text = "";
 
+    public UITextBox() {
+        super(true);
+        this.text = text;
+    }
+
     private void setText(String text) {
         this.text = text;
     }
