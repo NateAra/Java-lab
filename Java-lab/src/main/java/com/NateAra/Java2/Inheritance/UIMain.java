926c9a4c08e8b538b16b1d38f52f54cb93b2cae7
NateAra <nate.arafayne@appliedtechnology.se>
2023-12-04 17:19:38 +0100

    output to overridden toString

--- Java-lab/src/main/java/com/NateAra/Java2/Inheritance/UIMain.java
+++ Java-lab/src/main/java/com/NateAra/Java2/Inheritance/UIMain.java
@@ -5,6 +5,7 @@ public class UIMain {
     public static void main(String[] args) {
         var control = new UITextBox();
         control.disable();
+        control.setText("Hello World");
         System.out.println(control.isEnabled());
     }
 }
