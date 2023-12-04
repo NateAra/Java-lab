7a02665e32a584e941aede9219210c8991b43294
NateAra <nate.arafayne@appliedtechnology.se>
2023-12-04 18:35:57 +0100

    Basic imp of comparing objects

--- Java-lab/src/main/java/com/NateAra/Java2/Inheritance/UIMain.java
+++ Java-lab/src/main/java/com/NateAra/Java2/Inheritance/UIMain.java
@@ -6,6 +6,14 @@ public class UIMain {
         var control = new UITextBox();
         control.disable();
         control.setText("Hello World");
-        System.out.println(control.isEnabled());
+//        System.out.println(control.isEnabled());
+
+        // Comparing Objects
+        var point1 = new Point(1, 2);
+        var point2 = new Point(1, 2);
+
+        System.out.println(point1.equals(point2));
+        System.out.println(point1.hashCode());
+        System.out.println(point2.hashCode());
     }
 }
