6a707b3f913b14755d53d49bbc079f2d42143a6d
NateAra <nate.arafayne@appliedtechnology.se>
2023-12-06 11:47:41 +0100

    Result of the tax implementation

--- Java-lab/src/main/java/com/NateAra/Java2/Interface/MainTaxCalculator.java
+++ Java-lab/src/main/java/com/NateAra/Java2/Interface/MainTaxCalculator.java
@@ -1,4 +1,11 @@
 package com.NateAra.Java2.Interface;
 
 public class MainTaxCalculator {
+
+    public static void main(String[] args) {
+
+        var calculator = new TaxCalculator2018(30_000);
+        var report = new TaxReport(calculator);
+        report.show();
+    }
 }
