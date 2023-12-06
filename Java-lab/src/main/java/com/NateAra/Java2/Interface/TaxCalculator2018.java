09daf35396c7924c4335c274cbe7e2e5066d0ea6
NateAra <nate.arafayne@appliedtechnology.se>
2023-12-06 09:41:31 +0100

    Made the taxableIncome final

--- Java-lab/src/main/java/com/NateAra/Java2/Interface/TaxCalculator2018.java
+++ Java-lab/src/main/java/com/NateAra/Java2/Interface/TaxCalculator2018.java
@@ -2,7 +2,7 @@ package com.NateAra.Java2.Interface;
 
 public class TaxCalculator2018 implements TaxCalculator{
 
-    private double taxableIncome;
+    private final double taxableIncome;
 
     public TaxCalculator2018(double taxableIncome) {
         this.taxableIncome = taxableIncome;
