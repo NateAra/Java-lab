2bb205380989c2c0e04fe8fe9dbff2c13a7dec7f
NateAra <nate.arafayne@appliedtechnology.se>
2023-12-07 14:37:55 +0100

    MyTube Project: - The project is about decoupling tightly coupled class using Interfaces

---
+++ Java-lab/src/main/java/com/NateAra/Java2/myTube/VideoDatabase.java
@@ -0,0 +1,10 @@
+package com.NateAra.Java2.myTube;
+
+public class VideoDatabase {
+    public void store(Video video) {
+        System.out.println("Storing video metadata in a SQL database...");
+        System.out.println("Title: " + video.getTitle());
+        System.out.println("File Name: " + video.getFileName());
+        System.out.println("Done!\n");
+    }
+}
