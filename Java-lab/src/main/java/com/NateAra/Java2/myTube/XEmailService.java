03c20154e78513465b37540cd04144677392e144
NateAra <nate.arafayne@appliedtechnology.se>
2023-12-07 15:49:26 +0100

    Extracted interface

---
+++ Java-lab/src/main/java/com/NateAra/Java2/myTube/XEmailService.java
@@ -0,0 +1,9 @@
+package com.NateAra.Java2.myTube;
+
+public class XEmailService implements EmailService {
+    @Override
+    public void sendEmail(User user) {
+        System.out.println("Notifying " + user.getEmail() + "...");
+        System.out.println("Done!\n");
+    }
+}
