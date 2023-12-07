7511e1a5ca8d5543626f6a88a556c7763492b206
NateAra <nate.arafayne@appliedtechnology.se>
2023-12-07 14:37:51 +0100

    MyTube Project: - The project is about decoupling tightly coupled class using Interfaces

---
+++ Java-lab/src/main/java/com/NateAra/Java2/myTube/User.java
@@ -0,0 +1,17 @@
+package com.NateAra.Java2.myTube;
+
+public class User {
+    private String email;
+
+    public User(String email) {
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
