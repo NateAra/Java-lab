e76d3c47d942c8c37d4a4a86376e8834cb338480
NateAra <nate.arafayne@appliedtechnology.se>
2023-12-18 11:45:56 +0100

    Experimenting with exceptions

---
+++ Java-lab/src/main/java/com/NateAra/Java3/exceptions/ExceptionsMain.java
@@ -0,0 +1,12 @@
+package com.NateAra.Java3.exceptions;
+
+
+public class ExceptionsMain {
+
+    public static void main(String[] args) {
+        var exceptions = new Exceptions();
+        exceptions.show();
+        exceptions.showMultiple();
+        exceptions.showMultipleDifferent();
+    }
+}
