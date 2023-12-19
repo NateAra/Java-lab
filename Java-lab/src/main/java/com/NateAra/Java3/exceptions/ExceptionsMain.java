cb26031d3ba738e09a70f78ab4653cbe1d9c7928
NateAra <nate.arafayne@appliedtechnology.se>
2023-12-19 09:40:54 +0100

    Experimenting with checked and unchecked Exceptions

--- Java-lab/src/main/java/com/NateAra/Java3/exceptions/ExceptionsMain.java
+++ Java-lab/src/main/java/com/NateAra/Java3/exceptions/ExceptionsMain.java
@@ -1,6 +1,8 @@
 package com.NateAra.Java3.exceptions;
 
 
+import java.io.IOException;
+
 public class ExceptionsMain {
 
     public static void main(String[] args) {
@@ -8,5 +10,17 @@ public class ExceptionsMain {
         exceptions.show();
         exceptions.showMultiple();
         exceptions.showMultipleDifferent();
+
+        // Should throw an IllegalArgumentException, if value is less than 0
+
+        var account = new Account();
+        account.deposit(1);
+
+        try {
+            account.deposit2(-1);
+        } catch (IOException e) {
+            System.out.println("Logging");
+        }
+
     }
 }
