d3894eb9d3e24da596b34addc1bb33de7cea410d
NateAra <nate.arafayne@appliedtechnology.se>
2024-01-02 14:44:28 +0100

    Basic uses of Generic

---
+++ Java-lab/src/main/java/com/NateAra/Java3/generics/GenericsMain.java
@@ -0,0 +1,8 @@
+package com.NateAra.Java3.generics;
+
+public class GenericsMain {
+    public static void main(String[] args) {
+        var list = new GenericList<Integer>();
+        list.add(1);
+    }
+}
