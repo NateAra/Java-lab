9afa74939eeb0c6ee035f26c3c36985f6bcf5be6
NateAra <nate.arafayne@appliedtechnology.se>
2024-01-08 16:36:11 +0100

    Working With some of the basic Data Structures, Arrays.

---
+++ Java-lab/src/main/java/com/NateAra/DataStructure/Arrays/ArrayMain.java
@@ -0,0 +1,12 @@
+package com.NateAra.DataStructure.Arrays;
+
+import java.util.Arrays;
+
+public class ArrayMain {
+
+    public static void main(String[] args) {
+        int[] numbers = { 10, 20, 30 };
+        System.out.println(numbers.length);
+        System.out.println(Arrays.toString(numbers));
+    }
+}
