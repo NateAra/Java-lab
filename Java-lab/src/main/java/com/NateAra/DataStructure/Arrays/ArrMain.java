14436d6e40d8b03521f43cc3925b301a32c3d7a3
NateAra <nate.arafayne@appliedtechnology.se>
2024-01-10 12:55:07 +0100

    Implemented round accesses for the array class for testing

---
+++ Java-lab/src/main/java/com/NateAra/DataStructure/Arrays/ArrMain.java
@@ -0,0 +1,14 @@
+package com.NateAra.DataStructure.Arrays;
+
+
+public class ArrMain {
+    public static void main(String[] args) {
+        Array number = new Array(4);
+        number.insert(1);
+        number.insert(2);
+        number.insert(3);
+        number.insert(4);
+        number.removeAt(0);
+        number.print();
+    }
+}
