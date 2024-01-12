a0e9a436c51dce2aa5d5b321c1b4ede0f8fa73a6
NateAra <nate.arafayne@appliedtechnology.se>
2024-01-12 12:01:55 +0100

    Changed the values of the array

--- Java-lab/src/main/java/com/NateAra/DataStructure/Arrays/ArrMain.java
+++ Java-lab/src/main/java/com/NateAra/DataStructure/Arrays/ArrMain.java
@@ -5,10 +5,11 @@ public class ArrMain {
     public static void main(String[] args) {
         Array number1 = new Array(4);
 
-        number1.insert(1);
-        number1.insert(1000);
-        number1.insert(3);
-        number1.insert(4);
+        number1.insert(10);
+        number1.insert(20);
+        number1.insert(30);
+        number1.insert(40);
+
 
         number1.removeAt(0);
 
