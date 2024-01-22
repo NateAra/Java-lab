ac6a0f70ab30124749d2f75dc7291605975c8da6
NateAra <nate.arafayne@appliedtechnology.se>
2024-01-22 18:58:46 +0100

    Added privet fields and todo`s

--- Java-lab/src/main/java/com/NateAra/DataStructure/LinkLists/Node.java
+++ Java-lab/src/main/java/com/NateAra/DataStructure/LinkLists/Node.java
@@ -1,4 +1,14 @@
 package com.NateAra.DataStructure.LinkLists;
 
 public class Node {
+    private int value;
+    private Node next;
+
+//    ToDo
+//    addFirst
+//    addLast
+//    deleteFirst
+//    deleteLast
+//    contains
+//    indexOf
 }
