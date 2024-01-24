836623f75fc27395f10917633ce25f265311a342
NateAra <nate.arafayne@appliedtechnology.se>
2024-01-24 14:35:04 +0100

    Refactored planning phase

--- Java-lab/src/main/java/com/NateAra/DataStructure/LinkLists/LinkedList.java
+++ Java-lab/src/main/java/com/NateAra/DataStructure/LinkLists/LinkedList.java
@@ -3,4 +3,12 @@ package com.NateAra.DataStructure.LinkLists;
 public class LinkedList {
     private Node first;
     private Node last;
+
+    //    ToDo
+    //    addFirst
+    //    addLast
+    //    deleteFirst
+    //    deleteLast
+    //    contains
+    //    indexOf
 }
