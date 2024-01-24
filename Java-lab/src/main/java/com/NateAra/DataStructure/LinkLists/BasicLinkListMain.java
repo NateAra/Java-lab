58e4228b0643b87848f0709bf259ff6c7d0c156f
NateAra <nate.arafayne@appliedtechnology.se>
2024-01-24 15:23:43 +0100

    Experimenting with basic LinkedList

---
+++ Java-lab/src/main/java/com/NateAra/DataStructure/LinkLists/BasicLinkListMain.java
@@ -0,0 +1,12 @@
+package com.NateAra.DataStructure.LinkLists;
+
+import java.util.LinkedList;
+
+public class BasicLinkListMain {
+    public static void main(String[] args) {
+        LinkedList<Integer> list = new LinkedList<>();
+        list.addLast(1);
+        list.addLast(0);
+        System.out.println(list);
+    }
+}
