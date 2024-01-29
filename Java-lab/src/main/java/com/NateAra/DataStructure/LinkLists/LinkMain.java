90e580be2c706beae96d47f68dbea7c366bfee2c
NateAra <nate.arafayne@appliedtechnology.se>
2024-01-29 12:57:17 +0100

    checking add first functionality

--- Java-lab/src/main/java/com/NateAra/DataStructure/LinkLists/LinkMain.java
+++ Java-lab/src/main/java/com/NateAra/DataStructure/LinkLists/LinkMain.java
@@ -2,6 +2,9 @@ package com.NateAra.DataStructure.LinkLists;
 
 public class LinkMain {
     public static void main(String[] args) {
-
+        MyLinkedList list = new MyLinkedList();
+        list.addFirst(5);
+        list.addFirst(10);
+        System.out.println(list);
     }
 }
