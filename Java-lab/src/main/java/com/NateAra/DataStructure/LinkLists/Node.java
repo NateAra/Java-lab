63d74055c29106d891515fb476500b16937060cc
NateAra <nate.arafayne@appliedtechnology.se>
2024-01-31 12:44:42 +0100

    Learning the concept of node in linked list

--- Java-lab/src/main/java/com/NateAra/DataStructure/LinkLists/Node.java
+++ Java-lab/src/main/java/com/NateAra/DataStructure/LinkLists/Node.java
@@ -4,8 +4,24 @@ public class Node {
     private int value;
     private Node next;
 
-    public Node(int value, Node next) {
+    public Node(int value) {
         this.value = value;
+        this.next = null;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
         this.next = next;
     }
 }
