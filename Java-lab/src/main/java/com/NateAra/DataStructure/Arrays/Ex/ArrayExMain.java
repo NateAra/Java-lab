a9e761521a2bd8e9ca7b53ed7cd80883709e1aa9
NateAra <nate.arafayne@appliedtechnology.se>
2024-01-31 13:33:28 +0100

    Made a main for my Exercises where i can reapply on and on

---
+++ Java-lab/src/main/java/com/NateAra/DataStructure/Arrays/Ex/ArrayExMain.java
@@ -0,0 +1,8 @@
+package com.NateAra.DataStructure.Arrays.Ex;
+
+public class ArrayExMain {
+    public static void main(String[] args) {
+        ArrayEx num = new ArrayEx(2);
+        num.insert(0);
+    }
+}
