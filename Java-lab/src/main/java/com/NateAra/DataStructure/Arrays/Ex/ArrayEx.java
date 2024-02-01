7c2c566c8bf6a97e3858ea357572a644f7fd07d4
NateAra <nate.arafayne@appliedtechnology.se>
2024-02-01 17:43:02 +0100

    Implemented dynamic array resizing for insert method

--- Java-lab/src/main/java/com/NateAra/DataStructure/Arrays/Ex/ArrayEx.java
+++ Java-lab/src/main/java/com/NateAra/DataStructure/Arrays/Ex/ArrayEx.java
@@ -20,4 +20,14 @@ public class ArrayEx {
         this.items = new int[length];
         this.count = 0;
     }
+
+    public void insert(int item) {
+        if (count == items.length) {
+            int[] newitem = new int[count * 2];
+
+            System.arraycopy(items, 0, newitem, 0, count);
+            items = newitem;
+        }
+        items[count++] = item;
+    }
 }
