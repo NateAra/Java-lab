69bf9363bb4814d3107334c08d5bccd628674a07
NateAra <nate.arafayne@appliedtechnology.se>
2024-02-02 13:31:34 +0100

    Using the methods

--- Java-lab/src/main/java/com/NateAra/DataStructure/Arrays/Ex/ArrayExMain.java
+++ Java-lab/src/main/java/com/NateAra/DataStructure/Arrays/Ex/ArrayExMain.java
@@ -2,7 +2,9 @@ package com.NateAra.DataStructure.Arrays.Ex;
 
 public class ArrayExMain {
     public static void main(String[] args) {
-        ArrayEx num = new ArrayEx(2);
+        ArrayEx num = new ArrayEx(5);
         num.insert(0);
+        num.insert(9);
+        num.printItems();
     }
 }
