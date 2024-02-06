451f68c5e9d567e87db9987368ec6366af252133
NateAra <nate.arafayne@appliedtechnology.se>
2024-02-06 11:35:56 +0100

    Largest item in action

--- Java-lab/src/main/java/com/NateAra/DataStructure/Arrays/Ex/ArrayExMain.java
+++ Java-lab/src/main/java/com/NateAra/DataStructure/Arrays/Ex/ArrayExMain.java
@@ -6,11 +6,13 @@ public class ArrayExMain {
         num.insert(0);
         num.insert(9);
         num.insert(5);
+        num.insert(25);
 
         num.removeAt(0);
 
         num.printItems();
         System.out.println(num.indexOf(2));
 
+        num.largestItemUsingStream();
     }
 }
