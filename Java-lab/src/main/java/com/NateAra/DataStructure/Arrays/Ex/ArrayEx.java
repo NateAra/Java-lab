9d424446ce344c277f5780c54696e5c02569667e
NateAra <nate.arafayne@appliedtechnology.se>
2024-02-06 11:00:21 +0100

    Added a method the returns the index of an element based on the element.

--- Java-lab/src/main/java/com/NateAra/DataStructure/Arrays/Ex/ArrayEx.java
+++ Java-lab/src/main/java/com/NateAra/DataStructure/Arrays/Ex/ArrayEx.java
@@ -49,4 +49,13 @@ public class ArrayEx {
         }
         count--;
     }
+
+    public int indexOf(int item) {
+        for (int i = 0; i < count; i++) {
+            if (item == items[i]) {
+                return i;
+            }
+        }
+        return -1;
+    }
 }
