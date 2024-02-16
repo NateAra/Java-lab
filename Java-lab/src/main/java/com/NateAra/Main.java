a7a73323cfd0479efe148e85426550686e05e3fb
NateAra <nate.arafayne@appliedtechnology.se>
2024-02-16 17:44:38 +0100

    Removing vowels using replaceAll

--- Java-lab/src/main/java/com/NateAra/Main.java
+++ Java-lab/src/main/java/com/NateAra/Main.java
@@ -1,18 +1,16 @@
 package com.NateAra;
 
 import java.util.Arrays;
+import java.util.Collections;
 
 public class Main {
     public static void main(String[] args) {
 
 
 
-        System.out.println(remove("Nate"));
-
     }
 
-    public int countSheeps(Boolean[] arrayOfSheeps) {
-        return Arrays.stream(arrayOfSheeps).filter(x -> x != null).filter(x -> x).toArray().length;
+    public static String disemvowel(String str) {
+        return str.replaceAll("(?i)[aeiou]", "");
     }
-
 }
