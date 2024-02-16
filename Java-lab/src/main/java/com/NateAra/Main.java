f0f1973157b415171ded15c535267cea0f5f7526
NateAra <nate.arafayne@appliedtechnology.se>
2024-02-16 12:25:13 +0100

    Neat switch case implementation

--- Java-lab/src/main/java/com/NateAra/Main.java
+++ Java-lab/src/main/java/com/NateAra/Main.java
@@ -2,6 +2,22 @@ package com.NateAra;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+
+        System.out.println();
+
     }
+
+    public static Integer basicMath(String op, int v1, int v2)
+    {
+        return switch (op) {
+            case "+" -> v1 + v2;
+            case "-" -> v1 - v2;
+            case "*" -> v1 * v2;
+            case "/" -> v1 / v2;
+            default -> 0;
+        };
+    }
+
 }
