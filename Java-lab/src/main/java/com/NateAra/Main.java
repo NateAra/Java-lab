9e7748eeb7f7930a8a190d55fa3cdaf82eb8f37f
NateAra <nate.arafayne@appliedtechnology.se>
2024-02-20 19:06:30 +0100

    Longest Common prefix

--- Java-lab/src/main/java/com/NateAra/Main.java
+++ Java-lab/src/main/java/com/NateAra/Main.java
@@ -6,27 +6,23 @@ public class Main {
 
         int[] items = {1, 2, 3, 4, 5};
 
-        System.out.println(searchInsert(items, 3));
+        System.out.println();
 
     }
 
-    public static int searchInsert(int[] nums, int target) {
+    public String longestCommonPrefix(String[] strs) {
+        String prefix = strs[0];
 
-        int left = 0;
-        int right = nums.length - 1;
+        for (int i = 0; i < strs.length; i++) {
+            while (strs[i].indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+            }
 
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-
-            if (nums[mid] == target) {
-                return mid;
-            } else if (nums[mid] < target) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
+            if (prefix.isEmpty()) {
+                return "";
             }
         }
-        return -1;
+        return prefix;
     }
 
 }
