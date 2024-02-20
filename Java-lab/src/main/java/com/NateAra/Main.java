7bcf2c52d499362adf793103aa6d0fdde66d41e5
NateAra <nate.arafayne@appliedtechnology.se>
2024-02-20 12:05:26 +0100

    Two sum using hashMap

--- Java-lab/src/main/java/com/NateAra/Main.java
+++ Java-lab/src/main/java/com/NateAra/Main.java
@@ -1,7 +1,7 @@
 package com.NateAra;
 
-import java.util.Arrays;
-import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
@@ -11,12 +11,16 @@ public class Main {
     }
 
     public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> numMap = new HashMap<>();
+
         for (int i = 0; i < nums.length; i++) {
-            for (int u = i + 1; u < nums.length; u++) {
-                if (nums[i] + nums[u] == target) {
-                    return new int[] {i, u};
-                }
+            int com = target - nums[i];
+
+            if (numMap.containsKey(com)) {
+                return new int[] {numMap.get(com), i};
             }
+
+            numMap.put(nums[i], i);
         }
         return null;
     }
