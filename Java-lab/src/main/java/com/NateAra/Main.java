1de36e63f9e932fb3613d3a517f40b9e8bddd988
NateAra <nate.arafayne@appliedtechnology.se>
2024-02-20 12:40:15 +0100

    search for target in an Array

--- Java-lab/src/main/java/com/NateAra/Main.java
+++ Java-lab/src/main/java/com/NateAra/Main.java
@@ -1,27 +1,25 @@
 package com.NateAra;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println();
+        int[] items = {5, 3, 2, 1, 4};
+
+        System.out.println(search(items, 3));
 
     }
 
-    public void moveZeroes(int[] nums) {
-        int place = 0;
-
-        for (int num : nums) {
-            if (num != 0) {
-                nums[place++] = num;
+    public static int search(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target) {
+                return i;
             }
         }
-
-        while (place < nums.length) {
-            nums[place++] = 0;
-        }
+        return -1;
     }
 
 }
