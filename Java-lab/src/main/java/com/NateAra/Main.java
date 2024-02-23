5455d6998b2a59c83661e06fd840b6df074f08eb
NateAra <nate.arafayne@appliedtechnology.se>
2024-02-23 15:17:33 +0100

    Removed unnecessary code

--- Java-lab/src/main/java/com/NateAra/Main.java
+++ Java-lab/src/main/java/com/NateAra/Main.java
@@ -1,26 +1,10 @@
 package com.NateAra;
 
 
-
-
 public class Main {
     public static void main(String[] args) {
 
-        int[] items = {1, 2, 3, 4};
 
-        System.out.println();
-
-    }
-
-    public static Integer basicMath(String op, int v1, int v2)
-    {
-        return switch (op) {
-            case "+" -> v1 + v2;
-            case "-" -> v1 - v2;
-            case "*" -> v1 * v2;
-            case "/" -> v1 / v2;
-            default -> null;
-        };
     }
 
 }
