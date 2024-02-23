192e899e3ae5cce191ef2fcadf057953a05dcd2f
NateAra <nate.arafayne@appliedtechnology.se>
2024-02-23 15:25:03 +0100

    Repeating RockPaperScissors

--- Java-lab/src/main/java/com/NateAra/Main.java
+++ Java-lab/src/main/java/com/NateAra/Main.java
@@ -4,7 +4,12 @@ package com.NateAra;
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println(rps("scissors", "rock"));
 
+    }
+
+    public static String rps(String p1, String p2) {
+        return p1.equals(p2) ? "Draw!" : "Player " + ("rockscissors scissorspaper paperrock".contains(p1 + p2) ? 1 : 2 ) + " won!";
     }
 
 }
