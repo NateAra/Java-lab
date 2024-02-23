b9f50015fe34df376c98991148163d3cfe3a7d84
NateAra <nate.arafayne@appliedtechnology.se>
2024-02-23 13:56:19 +0100

    Removed unused import

--- Java-lab/src/main/java/com/NateAra/Main.java
+++ Java-lab/src/main/java/com/NateAra/Main.java
@@ -1,10 +1,7 @@
 package com.NateAra;
 
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.OptionalDouble;
+
 
 public class Main {
     public static void main(String[] args) {
