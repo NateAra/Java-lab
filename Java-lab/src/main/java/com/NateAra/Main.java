41f4c2252f53bfb833b60296e7fd730d5bfdcacf
NateAra <nate.arafayne@appliedtechnology.se>
2024-02-24 14:12:34 +0100

    Repeating Count Sheep

--- Java-lab/src/main/java/com/NateAra/Main.java
+++ Java-lab/src/main/java/com/NateAra/Main.java
@@ -1,16 +1,33 @@
 package com.NateAra;
 
 
+import java.util.Collections;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println(solution("Nate"));
+        Boolean[] array1 = {true,  true,  true,  false,
+                true,  true,  true,  true ,
+                true,  false, true,  false,
+                true,  false, false, true ,
+                true,  true,  true,  true ,
+                false, false, true,  true };
+
+
+        System.out.println();
 
     }
 
-    public static String solution(String str) {
-        // Your code here...
-        return new StringBuilder(str).reverse().toString();
+    public int countSheeps(Boolean[] arrayOfSheeps) {
+        int count = 0;
+
+        for (int i = 0; i < arrayOfSheeps.length; i++) {
+            if (arrayOfSheeps[i]) {
+                count++;
+            }
+        }
+
+        return count;
     }
 
 }
