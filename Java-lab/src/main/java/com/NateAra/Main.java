47fe65f42d0e8f9557c2138328391c4d28d1d724
NateAra <nate.arafayne@appliedtechnology.se>
2024-02-26 16:13:34 +0100

    Repeating Square Every Digit

--- Java-lab/src/main/java/com/NateAra/Main.java
+++ Java-lab/src/main/java/com/NateAra/Main.java
@@ -3,12 +3,18 @@ package com.NateAra;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println(disemvowel("natE"));
+        System.out.println(squareDigits(9119));
 
     }
 
-    public static String disemvowel(String str) {
-        return str.replaceAll("(?i)[aAeEiIoOuU]", "");
+    public static int squareDigits(int n) {
+        String sNum = String.valueOf(n);
+        StringBuilder result = new StringBuilder();
+        sNum.chars()
+                .map(Character::getNumericValue)
+                .map(x -> x * x)
+                .forEach(result::append);
+        return Integer.parseInt(result.toString());
     }
 
 }
