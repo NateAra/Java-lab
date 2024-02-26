baf134280bc0a562f456c46972d82bcafbd7ccbe
NateAra <nate.arafayne@appliedtechnology.se>
2024-02-26 16:27:53 +0100

    Repeating How good are you really

--- Java-lab/src/main/java/com/NateAra/Main.java
+++ Java-lab/src/main/java/com/NateAra/Main.java
@@ -1,20 +1,16 @@
 package com.NateAra;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println(squareDigits(9119));
+//        System.out.println(highAndLow("5 4 3 2 1"));
 
     }
 
-    public static int squareDigits(int n) {
-        String sNum = String.valueOf(n);
-        StringBuilder result = new StringBuilder();
-        sNum.chars()
-                .map(Character::getNumericValue)
-                .map(x -> x * x)
-                .forEach(result::append);
-        return Integer.parseInt(result.toString());
+    public static boolean betterThanAverage(int[] classPoints, int yourPoints) {
+        return Arrays.stream(classPoints).average().orElse(0) < yourPoints;
     }
 
 }
