7579e703412e578994f106fedde07938c3816dbd
NateAra <nate.arafayne@appliedtechnology.se>
2024-02-26 16:03:16 +0100

    Repeating Disemvowel Trolls

--- Java-lab/src/main/java/com/NateAra/Main.java
+++ Java-lab/src/main/java/com/NateAra/Main.java
@@ -1,27 +1,14 @@
 package com.NateAra;
 
-
-import java.util.Arrays;
-import java.util.Collections;
-
 public class Main {
     public static void main(String[] args) {
-
-        Object[] array1 = {"hay", "junk", "hay", "hay", "moreJunk", "needle", "randomJunk"};
-
-
-        System.out.println();
+        
+        System.out.println(disemvowel("natE"));
 
     }
 
-    public static int getCount(String str) {
-        int count = 0;
-
-        for (char i : str.toCharArray()) {
-            count += i == 'a' || i == 'e' || i == 'i' || i == 'o' || i == 'u' ? 1 : 0;
-        }
-
-        return count;
+    public static String disemvowel(String str) {
+        return str.replaceAll("(?i)[aAeEiIoOuU]", "");
     }
 
 }
