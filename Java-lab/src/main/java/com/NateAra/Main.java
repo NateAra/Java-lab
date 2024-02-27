fd49a4cda1e72a71f387d0b9200165dc5999c20c
NateAra <nate.arafayne@appliedtechnology.se>
2024-02-27 14:11:57 +0100

    Repeating Vowel Count

--- Java-lab/src/main/java/com/NateAra/Main.java
+++ Java-lab/src/main/java/com/NateAra/Main.java
@@ -5,13 +5,18 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println(Arrays.toString(digitize(54321)));
+        System.out.println(getCount("abracadabra"));
 
     }
 
-    public static int[] digitize(long n) {
+    public static int getCount(String str) {
+        int count = 0;
 
-        return new StringBuilder().append(n).chars().map(Character:: getNumericValue).toArray();
+        for (char i : str.toCharArray()) {
+            count += 'a' == i || 'e' == i ||'i' == i ||'o' == i ||'u' == i ? 1 : 0;
+        }
+
+        return count;
     }
 
 }
