f8faeeca539d22fc34e0fabdea9098c3c9806604
NateAra <nate.arafayne@appliedtechnology.se>
2024-02-27 15:56:24 +0100

    Repeating Two sum LeetCode

--- Java-lab/src/main/java/com/NateAra/Main.java
+++ Java-lab/src/main/java/com/NateAra/Main.java
@@ -1,16 +1,29 @@
 package com.NateAra;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println(getCount("abracadabra"));
+        int[] ar = {2,7,11,15};
+
+        System.out.println(Arrays.toString(twoSum(ar, 9)));
 
     }
 
-    public static int getCount(String str) {
-        return str.replaceAll("[^aeiou]", "").length();
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> r = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int com = target - nums[i];
+            if (r.containsKey(com)) {
+                return new int[] {r.get(com), i};
+            }
+            r.put(nums[i], i);
+        }
+        return null;
     }
 
 }
