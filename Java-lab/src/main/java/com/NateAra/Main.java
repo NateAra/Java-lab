13c17a100d5bbecff4b6505a19f01895e5cf81f7
NateAra <nate.arafayne@appliedtechnology.se>
2024-02-28 12:46:55 +0100

    Cleaning main

--- Java-lab/src/main/java/com/NateAra/Main.java
+++ Java-lab/src/main/java/com/NateAra/Main.java
@@ -1,29 +1,11 @@
 package com.NateAra;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-
 public class Main {
     public static void main(String[] args) {
 
-        int[] ar = {2,7,11,15};
-
-        System.out.println(Arrays.toString(twoSum(ar, 9)));
+        System.out.println();
 
     }
 
-    public static int[] twoSum(int[] nums, int target) {
-        Map<Integer, Integer> r = new HashMap<>();
-
-        for (int i = 0; i < nums.length; i++) {
-            int com = target - nums[i];
-            if (r.containsKey(com)) {
-                return new int[] {r.get(com), i};
-            }
-            r.put(nums[i], i);
-        }
-        return null;
-    }
 
 }
