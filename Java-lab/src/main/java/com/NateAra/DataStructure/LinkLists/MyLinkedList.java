251909efe4aca5e67a31187164b651a49a56f0a6
NateAra <nate.arafayne@appliedtechnology.se>
2024-02-28 12:59:28 +0100

    Restarting MyLinkedList

--- Java-lab/src/main/java/com/NateAra/DataStructure/LinkLists/MyLinkedList.java
+++ Java-lab/src/main/java/com/NateAra/DataStructure/LinkLists/MyLinkedList.java
@@ -10,48 +10,5 @@ package com.NateAra.DataStructure.LinkLists;
 
 public class MyLinkedList {
 
-        if (first == null) {
-            last = newNode;
-        } else {
-            newNode.setNext(first);
-            first = newNode;
-        }
-    }
-
-    public void addLast(int value) {
-        Node newNode = new Node(value);
-
-
-        if (last == null) {
-            first = newNode;
-        } else {
-            newNode.setNext(last);
-            last = newNode;  // Updating the last reference to the new node
-        }
-    }
-
-    @Override
-    public String toString() {
-        // Check if the list is empty
-        if (first == null) {
-            return "[]";
-        }
-
-        StringBuilder result = new StringBuilder();
-        Node current = first;
-
-        // Loop through each node, appending its value to the result string
-        while (current != null) {
-            result.append(current.getValue());
-            current = current.getNext();
-
-            // If there is another node, append a comma for formatting
-            if (current != null) {
-                result.append(", ");
-            }
-        }
-
-        return result.toString();
-    }
 
 }
