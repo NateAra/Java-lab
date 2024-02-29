cb0b4e3033c4d34ae9aec28a89b2dfcf6e56571c
NateAra <nate.arafayne@appliedtechnology.se>
2024-02-29 17:15:47 +0100

    Testing indexOf method

--- Java-lab/src/main/java/com/NateAra/DataStructure/LinkLists/LinkMain.java
+++ Java-lab/src/main/java/com/NateAra/DataStructure/LinkLists/LinkMain.java
@@ -3,10 +3,11 @@ package com.NateAra.DataStructure.LinkLists;
 public class LinkMain {
     public static void main(String[] args) {
         var num = new MyLinkedList();
-        num.addLast(1);
         num.addLast(2);
-        num.addLast(3);
-        num.addFirst(0);
-        System.out.println(num.contains(1));
+        num.addLast(4);
+        num.addLast(6);
+
+
+        System.out.println(num.indexOf(6));
     }
 }
