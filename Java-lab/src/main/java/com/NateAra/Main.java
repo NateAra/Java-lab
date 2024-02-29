503484ad39f73c428189a54e189edf8790f4ec5a
NateAra <nate.arafayne@appliedtechnology.se>
2024-02-29 19:49:17 +0100

    Repeating highAndLow

--- Java-lab/src/main/java/com/NateAra/Main.java
+++ Java-lab/src/main/java/com/NateAra/Main.java
@@ -5,12 +5,28 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println();
+        System.out.println(highAndLow("8 3 -5 42 -1 0 0 -9 4 7 4 -4"));
 
     }
 
-    public static String findNeedle(Object[] haystack) {
-        return "found the needle at position " + Arrays.asList(haystack).indexOf("needle");
+    public static String highAndLow(String numbers) {
+        String[] sNum = numbers.split(" ");
+
+        int  large = Integer.parseInt(sNum[0]);
+        int small = large;
+
+        for (String s : sNum) {
+            int num = Integer.parseInt(s);
+
+            if (large < num) {
+                large = num;
+            }
+            if (small > num) {
+                small = num;
+            }
+        }
+
+        return large + " " + small;
     }
 
 }
