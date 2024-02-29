a637386f440e976a8da92d59adc12ea2f9c9832d
NateAra <nate.arafayne@appliedtechnology.se>
2024-02-29 10:59:40 +0100

    Removed unnecessary setters

--- Java-lab/src/main/java/com/NateAra/DataStructure/LinkLists/ListNode.java
+++ Java-lab/src/main/java/com/NateAra/DataStructure/LinkLists/ListNode.java
@@ -12,15 +12,7 @@ public class ListNode {
         return value;
     }
 
-    public void setValue(int value) {
-        this.value = value;
-    }
-
     public ListNode getNext() {
         return next;
-    }
-
-    public void setNext(ListNode next) {
-        this.next = next;
     }
 }
