cdeef9e92b27a9f4a2855de203a1e54429a02654
NateAra <nate.arafayne@appliedtechnology.se>
2024-03-05 11:45:44 +0100

    Refactoring

--- Java-lab/src/main/java/com/NateAra/DataStructure/Stack/StackMain.java
+++ Java-lab/src/main/java/com/NateAra/DataStructure/Stack/StackMain.java
@@ -3,7 +3,7 @@ package com.NateAra.DataStructure.Stack;
 
 public class StackMain {
     public static void main(String[] args) {
-        String str = "etan";
+        String str = "etaN";
 
         StackExercise reverser = new StackExercise();
         System.out.println(reverser.reverse(str));
