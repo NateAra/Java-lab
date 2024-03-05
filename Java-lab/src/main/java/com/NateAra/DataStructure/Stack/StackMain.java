4a2a061b393d241f91c7b2ff681b721bfd109db1
NateAra <nate.arafayne@appliedtechnology.se>
2024-03-05 11:45:00 +0100

    Testing the method to reverse a string using Stack

---
+++ Java-lab/src/main/java/com/NateAra/DataStructure/Stack/StackMain.java
@@ -0,0 +1,12 @@
+package com.NateAra.DataStructure.Stack;
+
+
+public class StackMain {
+    public static void main(String[] args) {
+        String str = "etan";
+
+        StackExercise reverser = new StackExercise();
+        System.out.println(reverser.reverse(str));
+    }
+
+}
