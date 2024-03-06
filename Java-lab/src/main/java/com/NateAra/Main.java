5817cbdb6170ea6a0736b0c816296aff4b1b75ed
NateAra <nate.arafayne@appliedtechnology.se>
2024-03-06 10:42:17 +0100

    Refactoring

--- Java-lab/src/main/java/com/NateAra/Main.java
+++ Java-lab/src/main/java/com/NateAra/Main.java
@@ -3,12 +3,8 @@ package com.NateAra;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println();
 
     }
 
-    public String longestCommonPrefix(String[] strs) {
-        return "Throw towel for today";
-    }
 
 }
