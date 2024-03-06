26da9c310b807e50f05ea9a8191846b4e434131d
NateAra <nate.arafayne@appliedtechnology.se>
2024-03-06 16:16:22 +0100

    Testing push method in myStack class

--- Java-lab/src/main/java/com/NateAra/DataStructure/Stack/StackMain.java
+++ Java-lab/src/main/java/com/NateAra/DataStructure/Stack/StackMain.java
@@ -4,6 +4,16 @@ package com.NateAra.DataStructure.Stack;
 public class StackMain {
     public static void main(String[] args) {
 
+        MyStack num = new MyStack();
+        num.push(10);
+        num.push(20);
+        num.push(40);
+        num.push(50);
+        num.push(60);
+        num.push(70);
+        num.push(80);
+        System.out.println(num);
+
     }
 
 }
