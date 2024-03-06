2f8ce2dbc4b376f3a2add5b0f1074fcfb2e65fe2
NateAra <nate.arafayne@appliedtechnology.se>
2024-03-06 14:43:21 +0100

    Refactoring code

--- Java-lab/src/main/java/com/NateAra/DataStructure/Stack/StackMain.java
+++ Java-lab/src/main/java/com/NateAra/DataStructure/Stack/StackMain.java
@@ -3,10 +3,7 @@ package com.NateAra.DataStructure.Stack;
 
 public class StackMain {
     public static void main(String[] args) {
-        String str = "[Nate[{}]]";
 
-        StackExercise balance = new StackExercise();
-        System.out.println(balance.balancedExpression(str));
     }
 
 }
