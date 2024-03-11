7d99260432e4e1fb29f9a6d308edf310bfe1f09b
NateAra <nate.arafayne@appliedtechnology.se>
2024-03-11 13:44:08 +0100

    Testing out method to reverse a queue integer only using add, remove and IsEmpty

--- Java-lab/src/main/java/com/NateAra/DataStructure/Queues/QueueExercise.java
+++ Java-lab/src/main/java/com/NateAra/DataStructure/Queues/QueueExercise.java
@@ -5,6 +5,16 @@ import java.util.Stack;
 
 public class QueueExercise {
 
+    public static void main(String[] args) {
+
+        Queue<Integer> num = new ArrayDeque<>();
+        num.add(10);
+        num.add(20);
+        num.add(30);
+        System.out.println(num);
+        reverse(num);
+        System.out.println(num);
+    }
 
     public static void reverse(Queue<Integer> queue) {
         // ToDo - Using Only (Add, remove, IsEmpty)
