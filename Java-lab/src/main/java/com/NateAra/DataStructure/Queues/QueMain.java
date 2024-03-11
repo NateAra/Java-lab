c993872cc8791d11e90a846ebf6ebc64970fce74
NateAra <nate.arafayne@appliedtechnology.se>
2024-03-11 17:00:09 +0100

    Clean up

--- Java-lab/src/main/java/com/NateAra/DataStructure/Queues/QueMain.java
+++ Java-lab/src/main/java/com/NateAra/DataStructure/Queues/QueMain.java
@@ -4,18 +4,6 @@ package com.NateAra.DataStructure.Queues;
 
 public class QueMain {
     public static void main(String[] args) {
-        ArrayQueue arr = new ArrayQueue(5);
-        arr.add(10);
-        arr.add(20);
-        arr.add(30);
-        arr.add(40);
-        arr.add(50);
-        System.out.println(arr);
-        arr.dequeue();
-        arr.dequeue();
-        System.out.println(arr);
-        arr.add(60);
-        arr.add(70);
-        System.out.println(arr);
+
     }
 }
