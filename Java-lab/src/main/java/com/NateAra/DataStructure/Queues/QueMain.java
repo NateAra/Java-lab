eb8e9abc187bff5675b09672ddee9fcc5f02d8a1
NateAra <nate.arafayne@appliedtechnology.se>
2024-03-11 14:17:31 +0100

    Testing out add and print methods

--- Java-lab/src/main/java/com/NateAra/DataStructure/Queues/QueMain.java
+++ Java-lab/src/main/java/com/NateAra/DataStructure/Queues/QueMain.java
@@ -1,18 +1,13 @@
 package com.NateAra.DataStructure.Queues;
 
 
-import java.util.ArrayDeque;
-import java.util.Queue;
 
 public class QueMain {
     public static void main(String[] args) {
 
-        Queue<Integer> queue = new ArrayDeque<>();
-        queue.add(10);
-        queue.add(20);
-        queue.add(30);
-        queue.add(40);
-        System.out.println(queue.peek());
-        System.out.println(queue.isEmpty());
+        ArrayQueue arr = new ArrayQueue();
+        arr.add(10);
+        arr.add(20);
+        System.out.println(arr.print());
     }
 }
