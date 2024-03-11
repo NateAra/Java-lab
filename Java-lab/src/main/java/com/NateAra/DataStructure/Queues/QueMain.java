0907459769ae1ad33af78cf1fbc2418120ee795e
NateAra <nate.arafayne@appliedtechnology.se>
2024-03-11 16:53:42 +0100

    Refactoring

--- Java-lab/src/main/java/com/NateAra/DataStructure/Queues/QueMain.java
+++ Java-lab/src/main/java/com/NateAra/DataStructure/Queues/QueMain.java
@@ -5,17 +5,6 @@ package com.NateAra.DataStructure.Queues;
 public class QueMain {
     public static void main(String[] args) {
 
-        ArrayQueue arr = new ArrayQueue();
-        arr.add(10);
-        arr.add(20);
-        arr.add(30);
-        arr.add(40);
-        arr.add(50);
-        arr.dequeue();
-        arr.dequeue();
-        arr.add(60);
-        arr.add(70);
-        System.out.println(arr);
-        System.out.println(arr.peek());
+
     }
 }
