b2c648efa399df45ab7ac3f8a1df7f40943ee24e
NateAra <nate.arafayne@appliedtechnology.se>
2024-03-11 14:04:57 +0100

    Added a constructor for initiating the fields

--- Java-lab/src/main/java/com/NateAra/DataStructure/Queues/QueMain.java
+++ Java-lab/src/main/java/com/NateAra/DataStructure/Queues/QueMain.java
@@ -1,9 +1,18 @@
 package com.NateAra.DataStructure.Queues;
 
 
+import java.util.ArrayDeque;
+import java.util.Queue;
 
 public class QueMain {
     public static void main(String[] args) {
 
+        Queue<Integer> queue = new ArrayDeque<>();
+        queue.add(10);
+        queue.add(20);
+        queue.add(30);
+        queue.add(40);
+        System.out.println(queue.peek());
+        System.out.println(queue.isEmpty());
     }
 }
