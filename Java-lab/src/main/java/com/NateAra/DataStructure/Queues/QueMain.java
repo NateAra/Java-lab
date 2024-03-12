2ba132c028cd62391197dd69e93cbfb2ae6ef28c
NateAra <nate.arafayne@appliedtechnology.se>
2024-03-12 18:35:09 +0100

    Testing methods in StackQueue

--- Java-lab/src/main/java/com/NateAra/DataStructure/Queues/QueMain.java
+++ Java-lab/src/main/java/com/NateAra/DataStructure/Queues/QueMain.java
@@ -1,9 +1,14 @@
 package com.NateAra.DataStructure.Queues;
 
 
-
 public class QueMain {
     public static void main(String[] args) {
+//        Stack<Integer> stack = new Stack<>();
+
+        StackQueue stack = new StackQueue();
+        stack.enqueue(10);
+        stack.enqueue(20);
+        System.out.println(stack);
 
     }
 }
