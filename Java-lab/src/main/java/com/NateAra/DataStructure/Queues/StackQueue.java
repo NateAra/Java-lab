60c1bacae60ad301bc39923c140e1cc293dd3364
NateAra <nate.arafayne@appliedtechnology.se>
2024-03-13 11:39:24 +0100

    Implemented a dequeue method using stack.

--- Java-lab/src/main/java/com/NateAra/DataStructure/Queues/StackQueue.java
+++ Java-lab/src/main/java/com/NateAra/DataStructure/Queues/StackQueue.java
@@ -19,7 +19,13 @@ public class StackQueue {
     }
 
     public void dequeue() {
-
+        while (!stack1.isEmpty()) {
+            stack2.push(stack1.pop());
+        }
+        stack2.pop();
+        while (!stack2.isEmpty()) {
+            stack1.push(stack2.pop());
+        }
     }
 
     @Override
