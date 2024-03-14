fb400367e8fe4cd307166e209f0845f9616fb9d5
NateAra <nate.arafayne@appliedtechnology.se>
2024-03-14 15:42:35 +0100

    Testing the add method

--- Java-lab/src/main/java/com/NateAra/DataStructure/Queues/QueMain.java
+++ Java-lab/src/main/java/com/NateAra/DataStructure/Queues/QueMain.java
@@ -4,6 +4,12 @@ package com.NateAra.DataStructure.Queues;
 public class QueMain {
     public static void main(String[] args) {
 
+        PriorityQueue pri = new PriorityQueue();
+        pri.add(3);
+        pri.add(2);
+        pri.add(4);
 
+
+        System.out.println(pri);
     }
 }
