33fa51b55c2d655a885520237c33e32420dca82a
NateAra <nate.arafayne@appliedtechnology.se>
2024-03-14 16:07:38 +0100

    Testing Remove method

--- Java-lab/src/main/java/com/NateAra/DataStructure/Queues/QueMain.java
+++ Java-lab/src/main/java/com/NateAra/DataStructure/Queues/QueMain.java
@@ -4,11 +4,12 @@ package com.NateAra.DataStructure.Queues;
 public class QueMain {
     public static void main(String[] args) {
 
-        PriorityQueue pri = new PriorityQueue();
+        PriorityQueueArr pri = new PriorityQueueArr();
         pri.add(3);
         pri.add(2);
+        pri.add(1);
         pri.add(4);
-
+        pri.remove();
 
         System.out.println(pri);
     }
