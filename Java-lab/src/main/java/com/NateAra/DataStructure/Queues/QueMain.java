d3b2c97b0377aa0bd7af22268e5f1d0991612bcf
NateAra <nate.arafayne@appliedtechnology.se>
2024-03-14 14:06:51 +0100

    Clean up

--- Java-lab/src/main/java/com/NateAra/DataStructure/Queues/QueMain.java
+++ Java-lab/src/main/java/com/NateAra/DataStructure/Queues/QueMain.java
@@ -4,19 +4,6 @@ package com.NateAra.DataStructure.Queues;
 public class QueMain {
     public static void main(String[] args) {
 
-        StackQueue stack = new StackQueue();
-        stack.enqueue(10);
-        stack.enqueue(20);
-        stack.enqueue(30);
-        stack.enqueue(40);
-        stack.enqueue(50);
-        stack.dequeue2();
-        stack.dequeue2();
-        stack.enqueue(60);
-        stack.enqueue(70);
-        stack.dequeue2();
-        System.out.println(stack);
-        System.out.println(stack.peek());
 
     }
 }
