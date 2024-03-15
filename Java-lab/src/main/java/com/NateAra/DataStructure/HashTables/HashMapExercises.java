e59e2aa53ee60043c7ef5458ac773c912706de83
NateAra <nate.arafayne@appliedtechnology.se>
2024-03-15 12:27:02 +0100

    Exercises using hashMap

---
+++ Java-lab/src/main/java/com/NateAra/DataStructure/HashTables/HashMapExercises.java
@@ -0,0 +1,12 @@
+package com.NateAra.DataStructure.HashTables;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMapExercises {
+    public static void main(String[] args) {
+        Map<Integer, String> map = new HashMap<>();
+        map.put(1, "Nate");
+        System.out.println(map);
+    }
+}
