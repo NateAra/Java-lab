2a591cbbe65c783d6ae65ab4e5c60219aed96c33
NateAra <nate.arafayne@appliedtechnology.se>
2024-03-15 12:24:43 +0100

    Working With hashMaps

---
+++ Java-lab/src/main/java/com/NateAra/DataStructure/HashTables/HashTableMain.java
@@ -0,0 +1,12 @@
+package com.NateAra.DataStructure.HashTables;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashTableMain {
+    public static void main(String[] args) {
+        Map<Integer, String> t = new HashMap<>();
+        t.put(1, "Nate");
+        System.out.println(t);
+    }
+}
