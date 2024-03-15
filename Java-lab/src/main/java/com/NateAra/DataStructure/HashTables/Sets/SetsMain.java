f92f8eb2c7302d40a8652c1ef012618ffeb351eb
NateAra <nate.arafayne@appliedtechnology.se>
2024-03-15 16:24:57 +0100

    Implemented a method to remove Repeated nums

--- Java-lab/src/main/java/com/NateAra/DataStructure/HashTables/Sets/SetsMain.java
+++ Java-lab/src/main/java/com/NateAra/DataStructure/HashTables/Sets/SetsMain.java
@@ -1,4 +1,13 @@
 package com.NateAra.DataStructure.HashTables.Sets;
 
 public class SetsMain {
+
+
+    public static int[] removeRepeatingNum(int[] array) {
+        Set<Integer> num = new HashSet<>();
+        for (int i : array) {
+            num.add(i);
+        }
+        return num.stream().mapToInt(Integer::intValue).toArray();
+    }
 }
