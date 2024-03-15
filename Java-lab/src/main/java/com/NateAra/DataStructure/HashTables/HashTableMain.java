a44d40e11ec12c1c64b97703e713157faaf0bffe
NateAra <nate.arafayne@appliedtechnology.se>
2024-03-15 13:21:18 +0100

    Exercises find First Non-Repeating Char

--- Java-lab/src/main/java/com/NateAra/DataStructure/HashTables/HashTableMain.java
+++ Java-lab/src/main/java/com/NateAra/DataStructure/HashTables/HashTableMain.java
@@ -9,4 +9,28 @@ public class HashTableMain {
         t.put(1, "Nate");
         System.out.println(t);
     }
+
+    public static void findFirstNonRepeatingChar(String str) {
+
+        Map<Character, Integer> map = new HashMap<>();
+
+        for (var ch : str.toCharArray()) {
+            if (map.containsKey(ch)) {
+                var count = map.get(ch);
+                map.put(ch, count + 1);
+            } else {
+                map.put(ch, 1);
+            }
+        }
+
+        for (var ch : str.toCharArray()) {
+            if (map.get(ch) == 1) {
+                System.out.println("The first non-repeating character is: " + ch);
+                return;
+            }
+        }
+        System.out.println("No non-repeating chars");
+    }
+
+
 }
