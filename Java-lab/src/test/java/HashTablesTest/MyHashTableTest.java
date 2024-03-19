91a475748c8c27e65967f41859ca6698527ca953
NateAra <nate.arafayne@appliedtechnology.se>
2024-03-19 15:08:23 +0100

    Implementing put&get test

--- Java-lab/src/test/java/HashTablesTest/MyHashTableTest.java
+++ Java-lab/src/test/java/HashTablesTest/MyHashTableTest.java
@@ -7,5 +7,15 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MyHashTableTest {
 
+    @Test
+    void putAndGet() {
+        MyHashTable hashTable = new MyHashTable();
+        hashTable.put(1, "One");
+        hashTable.put(2, "Two");
+        hashTable.put(3, "Three");
 
+        assertEquals("One", hashTable.get(1));
+        assertEquals("Two", hashTable.get(2));
+        assertEquals("Three", hashTable.get(3));
+    }
 }
