a49206aad622341eb5a14836aebf035b8836dadb
NateAra <nate.arafayne@appliedtechnology.se>
2024-03-19 15:07:39 +0100

    Implementing JUnite tests

---
+++ Java-lab/src/test/java/HashTablesTest/MyHashTableTest.java
@@ -0,0 +1,11 @@
+package HashTablesTest;
+
+import com.NateAra.DataStructure.HashTables.MyHashTable;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MyHashTableTest {
+
+
+}
