a4699088d9051e204bbd43cea755d890ef2a3b7b
NateAra <nate.arafayne@appliedtechnology.se>
2024-03-19 11:48:01 +0100

    Testing the get() method

--- Java-lab/src/main/java/com/NateAra/DataStructure/HashTables/HashTableMain.java
+++ Java-lab/src/main/java/com/NateAra/DataStructure/HashTables/HashTableMain.java
@@ -1,10 +1,12 @@
 package com.NateAra.DataStructure.HashTables;
 
-
-
 public class HashTableMain {
     public static void main(String[] args) {
 
+        MyHashTable table = new MyHashTable();
+        table.put(22, "Nate");
+        table.put(11, "Johny");
+        System.out.println(table.get(11));
     }
 
 }
