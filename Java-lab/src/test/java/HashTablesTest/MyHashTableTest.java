24bcf5a0bb74e119ec57a36b02e1746ec1ad292a
NateAra <nate.arafayne@appliedtechnology.se>
2024-03-19 15:19:48 +0100

    Refactored

--- Java-lab/src/test/java/HashTablesTest/MyHashTableTest.java
+++ Java-lab/src/test/java/HashTablesTest/MyHashTableTest.java
@@ -4,6 +4,7 @@ import com.NateAra.DataStructure.HashTables.MyHashTable;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class MyHashTableTest {
 
