16ab0d7102e37afa79a2667b5571675846d1a758
NateAra <nate.arafayne@appliedtechnology.se>
2024-03-21 15:51:52 +0100

    Implementing insert using recursive method

--- Java-lab/src/main/java/com/NateAra/DataStructure/Part2/BinaryTrees/BinaryTree.java
+++ Java-lab/src/main/java/com/NateAra/DataStructure/Part2/BinaryTrees/BinaryTree.java
@@ -2,4 +2,17 @@ package com.NateAra.DataStructure.Part2.BinaryTrees;
 
 public class BinaryTree {
     private BinaryTreeNode root;
+
+    public BinaryTree() {
+        this.root = null;
+    }
+
+    public void insertUsingRecursive(int value) {
+        root = insertRecursive(root, value);
+    }
+
+    private BinaryTreeNode insertRecursive(BinaryTreeNode current, int value) {
+        return null;
+    }
+
 }
