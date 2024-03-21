e7d3f46bf75e712200ad37200846972ead7882ad
NateAra <nate.arafayne@appliedtechnology.se>
2024-03-21 11:58:25 +0100

    Implementing BinaryTreeNode fields

--- Java-lab/src/main/java/com/NateAra/DataStructure/Part2/BinaryTrees/BinaryTreeNode.java
+++ Java-lab/src/main/java/com/NateAra/DataStructure/Part2/BinaryTrees/BinaryTreeNode.java
@@ -1,5 +1,9 @@
 package com.NateAra.DataStructure.Part2.BinaryTrees;
 
 public class BinaryTreeNode {
+    private int value;
+    private BinaryTreeNode left;
+    private BinaryTreeNode right;
+
 
 }
