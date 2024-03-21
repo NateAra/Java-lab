7e4233deccc2106210a801a0c686847deb32c64a
NateAra <nate.arafayne@appliedtechnology.se>
2024-03-21 11:58:40 +0100

    Implementing BinaryTreeNode constructor

--- Java-lab/src/main/java/com/NateAra/DataStructure/Part2/BinaryTrees/BinaryTreeNode.java
+++ Java-lab/src/main/java/com/NateAra/DataStructure/Part2/BinaryTrees/BinaryTreeNode.java
@@ -5,5 +5,9 @@ public class BinaryTreeNode {
     private BinaryTreeNode left;
     private BinaryTreeNode right;
 
-
+    public BinaryTreeNode(int value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
 }
