6bf195f8807619d005335a80e7e8251cc6651159
NateAra <nate.arafayne@appliedtechnology.se>
2024-03-22 14:50:49 +0100

    Testing my method

--- Java-lab/src/main/java/com/NateAra/DataStructure/Part2/BinaryTrees/BinaryTreeMain.java
+++ Java-lab/src/main/java/com/NateAra/DataStructure/Part2/BinaryTrees/BinaryTreeMain.java
@@ -1,4 +1,13 @@
 package com.NateAra.DataStructure.Part2.BinaryTrees;
 
 public class BinaryTreeMain {
+    public static void main(String[] args) {
+        BinaryTree bin = new BinaryTree();
+        bin.insertUsingRecursive(5);
+        bin.insertUsingRecursive(2);
+        bin.insertUsingRecursive(4);
+        bin.insertUsingRecursive(8);
+        System.out.println(bin.find(1));
+        System.out.println(bin.findWithRecursive(4));
+    }
 }
