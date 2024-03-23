1cd4c35c8384a659717eb2b665d4ea3ec8191aca
NateAra <nate.arafayne@appliedtechnology.se>
2024-03-23 16:19:18 +0100

    Refactored

--- Java-lab/src/main/java/com/NateAra/DataStructure/Part2/BinaryTrees/BinaryTreeMain.java
+++ Java-lab/src/main/java/com/NateAra/DataStructure/Part2/BinaryTrees/BinaryTreeMain.java
@@ -2,14 +2,6 @@ package com.NateAra.DataStructure.Part2.BinaryTrees;
 
 public class BinaryTreeMain {
     public static void main(String[] args) {
-        BinaryTree bin = new BinaryTree();
-        bin.insertUsingRecursive(5);
-        bin.insertUsingRecursive(3);
-        bin.insertUsingRecursive(7);
-        bin.insertUsingRecursive(4);
-        bin.insertUsingRecursive(2);
-        bin.insertUsingRecursive(8);
 
-        bin.postOrderTraversal();
     }
 }
