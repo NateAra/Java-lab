f5d38b5f3459b3095c08c35aabe467a39c84a73c
NateAra <nate.arafayne@appliedtechnology.se>
2024-03-23 16:12:29 +0100

    Testing In-order Traversal method

--- Java-lab/src/main/java/com/NateAra/DataStructure/Part2/BinaryTrees/BinaryTreeMain.java
+++ Java-lab/src/main/java/com/NateAra/DataStructure/Part2/BinaryTrees/BinaryTreeMain.java
@@ -4,11 +4,12 @@ public class BinaryTreeMain {
     public static void main(String[] args) {
         BinaryTree bin = new BinaryTree();
         bin.insertUsingRecursive(5);
-        bin.insertUsingRecursive(2);
+        bin.insertUsingRecursive(3);
+        bin.insertUsingRecursive(7);
         bin.insertUsingRecursive(4);
+        bin.insertUsingRecursive(2);
         bin.insertUsingRecursive(8);
-        System.out.println(bin.find(1));
-        System.out.println(bin.findWithRecursive(4));
-        bin.preOrderTraversal();
+
+        bin.inOrderTraversal();
     }
 }
