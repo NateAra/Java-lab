ed92dc0e8b06381eac484434824d75a152fc8204
NateAra <nate.arafayne@appliedtechnology.se>
2024-03-25 12:22:16 +0100

    Testing height method

--- Java-lab/src/main/java/com/NateAra/DataStructure/Part2/BinaryTrees/BinaryTreeMain.java
+++ Java-lab/src/main/java/com/NateAra/DataStructure/Part2/BinaryTrees/BinaryTreeMain.java
@@ -2,6 +2,13 @@ package com.NateAra.DataStructure.Part2.BinaryTrees;
 
 public class BinaryTreeMain {
     public static void main(String[] args) {
-
+        BinaryTree bin = new BinaryTree();
+        bin.insert(5);
+        bin.insert(7);
+        bin.insert(3);
+        bin.insert(8);
+        bin.insert(2);
+        bin.insert(6);
+        System.out.println(bin.height());
     }
 }
