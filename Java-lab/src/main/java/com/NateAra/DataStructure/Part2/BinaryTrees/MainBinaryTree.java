c42afb20e064e19feaf644bf69c17e4417d756cb
NateAra <nate.arafayne@appliedtechnology.se>
2024-03-26 21:50:02 +0100

    testing min method

--- Java-lab/src/main/java/com/NateAra/DataStructure/Part2/BinaryTrees/MainBinaryTree.java
+++ Java-lab/src/main/java/com/NateAra/DataStructure/Part2/BinaryTrees/MainBinaryTree.java
@@ -3,12 +3,13 @@ package com.NateAra.DataStructure.Part2.BinaryTrees;
 public class MainBinaryTree {
     public static void main(String[] args) {
         BinaryTree bin = new BinaryTree();
-        bin.insert(5);
-        bin.insert(7);
-        bin.insert(3);
+        bin.insert(4);
+        bin.insert(9);
+        bin.insert(1);
+        bin.insert(12);
         bin.insert(8);
         bin.insert(2);
         bin.insert(6);
-        System.out.println(bin.height());
+        System.out.println(bin.min());
     }
 }
