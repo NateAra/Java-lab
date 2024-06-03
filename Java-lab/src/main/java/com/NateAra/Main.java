e4c237c0ab5da0838b0e0cdc0515be3da4b24598
NateAra <nate.arafayne@appliedtechnology.se>
2024-06-03 19:21:33 +0200

    LeetCode Exercise: 58 Length of Last Word

--- Java-lab/src/main/java/com/NateAra/Main.java
+++ Java-lab/src/main/java/com/NateAra/Main.java
@@ -2,9 +2,14 @@ package com.NateAra;
 
 public class Main {
     public static void main(String[] args) {
-
+        String word = "Hello World";
+        System.out.println(lengthOfLastWord(word));
 
     }
 
+    public static int lengthOfLastWord(String s) {
+        String str[] = s.split(" ");
+        return str[str.length - 1].length();
+    }
 
 }
