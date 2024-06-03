9a2047eb62b6d2de085ab8ee046cd1af5dd43553
NateAra <nate.arafayne@appliedtechnology.se>
2024-06-03 19:57:43 +0200

    LeetCode Exercise: 20. Valid Parentheses

--- Java-lab/src/main/java/com/NateAra/Main.java
+++ Java-lab/src/main/java/com/NateAra/Main.java
@@ -1,15 +1,32 @@
 package com.NateAra;
 
+import java.util.Stack;
+
 public class Main {
     public static void main(String[] args) {
-        String word = "Hello World";
-        System.out.println(lengthOfLastWord(word));
+        String word = "(){}";
+        System.out.println(isValid(word));
 
     }
 
-    public static int lengthOfLastWord(String s) {
-        String str[] = s.split(" ");
-        return str[str.length - 1].length();
+    public static boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                stack.push(')');
+            } else if (c == '{') {
+                stack.push('}');
+            } else if (c == '[') {
+                stack.push(']');
+            } else {
+                if (stack.isEmpty() || stack.pop() != c) {
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();
     }
 
 }
